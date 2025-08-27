5974f406bc36da57a4e999ffec5988c4253d85aa
wfayoz <wfayoz@yahoo.com>
2025-08-27 10:38:35 +0500

    add sqlalchemy settings

--- database.py
+++ database.py
@@ -1,1 +1,2 @@
 # connect sqlalchemy
+import sqlalchemy
--- main.py
+++ main.py
@@ -11,3 +11,31 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+"""
+ctrl + t
+command + t (mac)
+pull
+merge
+
+
+ctrl + k
+command + k (mac)
+commit
+
+ctrl + shift + k
+command + shift + k (mac)
+push
+
+
+git
+github, gitlab, bitbucket
+github cicd, gitlab cicd
+
+branch 
+
+features
+
+
+"""
