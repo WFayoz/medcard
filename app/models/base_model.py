662f820a27c66add382a440e6e2c477cf98d0051
glrkh <gulrukhkhalilovna@gmail.com>
2025-08-29 12:05:09 +0500

    base model added

---
+++ app/models/base_model.py
@@ -0,0 +1,115 @@
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy import DateTime, func, update as sqlalchemy_update, select, delete as sqlalchemy_delete
+from sqlalchemy.ext.asyncio import AsyncAttrs, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr, selectinload
+from sqlalchemy.sql import text
+
+from core.config import settings
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    @declared_attr
+    def __tablename__(self):
+        _name = self.__name__
+        _new_name = _name[0]
+        for i in _name[1:]:
+            if i.isupper():
+                _new_name += '_'
+            _new_name += i
+        if _new_name[-1] == 'y':
+            _new_name = _new_name[:-1] + 'ies'
+        else:
+            _new_name += 's'
+        return _new_name.lower()
+
+
+class Database:
+
+    def __init__(self):
+        self._engine = None
+        self._session = None
+
+    def init(self):
+        self._engine = create_async_engine(settings.postgres_async_url)
+        self._session = async_sessionmaker(self._engine, expire_on_commit=False)()
+
+    def __getattr__(self, name):
+        return getattr(self._session, name)
+
+    async def create_all(self):
+        async with self._engine.begin() as engine:
+            await engine.run_sync(Base.metadata.create_all)
+
+    async def drop_all(self):
+        async with self._engine.begin() as engine:
+            await engine.run_sync(Base.metadata.drop_all)
+
+
+db = Database()
+db.init()
+
+
+class AbstractClass:
+    @staticmethod
+    async def commit():
+        try:
+            await db.commit()
+        except Exception as e:
+            print(e)
+            await db.rollback()
+
+    @classmethod
+    async def create(cls, **kwargs):  # Create
+        object_ = cls(**kwargs)
+        db.add(object_)
+        await cls.commit()
+        return object_
+
+    @classmethod
+    async def update(cls, id_, **kwargs):
+        query = (
+            sqlalchemy_update(cls)
+            .where(cls.id == id_)
+            .values(**kwargs)
+            .execution_options(synchronize_session="fetch")
+            .returning(cls)
+        )
+        new_obj = await db.execute(query)
+        await cls.commit()
+        return new_obj.scalar()
+
+    @classmethod
+    async def get(cls, id_):
+        query = select(cls).where(cls.id == id_)
+        return (await db.execute(query)).scalar()
+
+    @classmethod
+    async def delete(cls, id_):
+        query = sqlalchemy_delete(cls).where(cls.id == id_)
+        await db.execute(query)
+        await cls.commit()
+
+    @classmethod
+    async def get_all(cls):
+        return (await db.execute(select(cls))).scalars()
+
+    @classmethod
+    async def filter(cls, criteria, *, relationship=None):
+        query = select(cls).where(criteria)
+        if relationship:
+            query = query.options(selectinload(relationship))
+
+        return (await db.execute(query)).scalars().all()
+
+
+class Model(Base, AbstractClass):
+    __abstract__ = True
+    id: Mapped[str] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        server_default=text("gen_random_uuid()")
+    )
+    updated_at: Mapped[datetime] = mapped_column(DateTime, server_onupdate=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
---
+++ app/models/clinics.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.base_model import Model
+
+
+class Clinic(Model):
+    name : Mapped[str] = mapped_column(unique=True)
+    phone : Mapped[str] = mapped_column(String(15),unique=True)
+    email : Mapped[str] = mapped_column(String,unique=True)
+    website : Mapped[str] = mapped_column(String,unique=True)
+    location : Mapped[str] = mapped_column(String(255))
+    medowner_id : Mapped[str] = mapped_column(ForeignKey('user.id'), unique=True)
+    medowner = relationship('User', foreign_keys=[medowner_id])
