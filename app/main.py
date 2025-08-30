e26cf19a7ffb40e26ff50785392beac0ad93d1f2
glrkh <gulrukhkhalilovna@gmail.com>
2025-08-30 16:36:13 +0500

    admin panel for clinics

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from starlette_admin.contrib.sqla import Admin
-from admin.models import ClinicAdmin
+from app.admin.models import ClinicAdmin
 from app.models import Clinic
 from app.models.base_model import db
 
