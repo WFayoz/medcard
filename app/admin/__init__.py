f78779597e280952119fd31a1577d22a5bc97f91
glrkh <gulrukhkhalilovna@gmail.com>
2025-09-05 09:31:55 +0500

    admin panel for clinics

---
+++ app/admin/__init__.py
@@ -0,0 +1,9 @@
+from starlette_admin.contrib.sqla import Admin
+
+from app.admin.models import ClinicAdmin
+from app.models import Clinic
+from app.models.base_model import db
+
+admin = Admin(engine=db.engine, title='Clinics Administration')
+
+admin.add_view(ClinicAdmin(Clinic))
